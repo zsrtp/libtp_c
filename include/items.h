cdc7cdadcb9b9d78a69b2f02e1a2976dd66cf563
unknown <pheenoh@gmail.com>
2020-01-26 18:58:49 -0500

    fix padding to use hex

--- include/items.h
+++ include/items.h
@@ -6,17 +6,17 @@
 
 namespace Clawshot {
     struct ClawshotProperty {
-        uint8_t _p0[4];                         //8038E9C0
+        uint8_t _p0[0x04];                         //8038E9C0
         uint16_t arm_animation1;                //8038E9C4 affects first person anim while firing
-        uint8_t _p1[2];                         //8038E9C6
+        uint8_t _p1[0x02];                         //8038E9C6
         uint16_t arm_animation2;                //8038E9C8 affects first person anim while firing
-        uint8_t _p2[22];                        //8038E9CA
+        uint8_t _p20[0x22];                        //8038E9CA
         uint16_t wall_hang_anim_speed;          //8038E9EC
-        uint8_t _p3[2];                         //8038E9EE
+        uint8_t _p3[0x02];                         //8038E9EE
         uint16_t wall_hang_anim;                //8038E9F0 has something to do with wall anim but not speed specifically
-        uint8_t _p4[6];                         //8038EAF2 
+        uint8_t _p4[0x06];                         //8038EAF2 
         uint16_t second_clawshot_wait_frames;   //8038EAF8 how long before you can use clawshot 2 when hanging from wall (ceiling is different) 
-        uint8_t _p5[6];                         //8038EAFA
+        uint8_t _p5[0x06];                         //8038EAFA
         uint16_t third_person_ready_anim_angle; //8038EA00 data type is wrong probably
         uint16_t third_person_ready_anim_speed; //8038EA02 data type is wrong probably
         float first_person_ready_anim_frames;   //8038EA04
