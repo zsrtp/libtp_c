8ef3c0c21cab5e63ad69b15073e3c1b53902f7b7
unknown <pheenoh@gmail.com>
2020-01-26 18:46:43 -0500

    begin adding super clawshot cheat

--- src/cheats.cpp
+++ src/cheats.cpp
@@ -1,9 +1,12 @@
 #include "cheats.h"
 #include "controller.h"
 #include "tp.h"
+#include "items.h"
 
 namespace Cheats {
     using namespace Controller;
+    using namespace Clawshot;
+
     void apply_cheats() {
         if (tp_mPadStatus.sval == (Pad::R | Pad::A)) {
             if (tp_gameInfo.momentum_ptr) {
@@ -14,5 +17,10 @@ namespace Cheats {
             tp_mPadButton.sval = 0x0;
             tp_mPadStatus.sval = 0x0;
         }
+
+        Clawshot::set_speed(100.0);
+        Clawshot::set_pull_rate(60.0);
+        Clawshot::set_extension_rate(2000.0);
+        Clawshot::set_retraction_rate(150.0);          
     }
 }  // namespace Cheats
