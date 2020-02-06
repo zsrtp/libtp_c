89f73d85ce1ca359ffedb3ad8c306789ab1d999e
Cristobal Hall-Ramos <crishallramos@yahoo.com>
2020-02-05 20:37:39 -0500

    Add Graphics Addresses

--- include/addrs.h
+++ include/addrs.h
@@ -35,4 +35,38 @@
 // Inventory
 #define tp_execItemGet_addr 0x80097e8c
 
+// GX
+#define GXSetBlendMode_addr 0x8035FBF0
+#define GXBegin_addr 0x8035C764
+#define GXSetVtxAttrFmt_addr 0x8035B5C4
+#define GXLoadPosMtxImm_addr 0x8036024C
+#define GXSetNumIndStages_addr 0x8035F0D4
+#define GXSetTevDirect_addr 0x8035F0F8
+#define GXSetAlphaCompare_addr 0x8035F624
+#define GXSetZMode_addr 0x8035FC9C
+#define GXSetTevOp_addr 0x8035F198
+#define GXSetNumChans_addr 0x8035DB30
+#define GXSetNumTevStages_addr 0x8035F890
+#define GXSetNumTexGens_addr 0x8035BDFC
+#define GXSetTevOrder_addr 0x8035F6F4
+#define GXSetTevColorIn_addr 0x8035F224
+#define GXSetTevAlphaIn_addr 0x8035F268
+#define GXSetTevColorOp_addr 0x8035F2AC
+#define GXSetTevAlphaOp_addr 0x8035F314
+#define GXSetCullMode_addr 0x8035C984
+#define GXLoadTexMtxImm_addr 0x80360320
+#define GXSetChanCtrl_addr 0x8035DB6C
+#define GXSetCurrentMtx_addr 0x803602EC
+#define GXSetTexCoordGen2_addr 0x8035BB7C
+#define GXSetLineWidth_addr 0x8035C8BC
+#define GXClearVtxDesc_addr 0x8035B58C
+#define GXSetVtxDesc_addr 0x8035AEB8
+#define GXInitTexObj_addr 0x8035DE40
+#define GXLoadTexObj_addr 0x8035E414
+#define GXInvalidateTexAll_addr 0x8035E664
+#define GXSetProjection_addr 0x803600D4
+#define GXSetScissor_addr 0x803604D0
+#define GXGetScissor_addr 0x80360548
+#define wgPipe_addr 0xCC008000
+
 #endif  // LIB_TP_ADDRS
